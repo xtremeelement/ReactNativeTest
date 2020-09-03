d3cb641ef09e0132562c7f1ecff139a96d8451b4
Web <Web@Web-Dev.local>
2020-09-03 11:00:29 -0400

    starting fresh

--- App.js
+++ App.js
@@ -4,66 +4,10 @@ import { StyleSheet, Text, View, TextInput, Button, FlatList, TouchableOpacity }
 
 export default function App() {
 
-const [password, setPassword] = useState("");
-const [username, setUsername] = useState("");
-const [users, setUsers] = useState([]);
-
-useEffect(()=>{
-  const url = "https://jsonplaceholder.typicode.com/users"
-  fetch(url)
-  .then(response => response.json())
-  .then(json => setUsers(json));
-},[])
-
-const pressedButton = (id) => {
-  alert(`pressed item: ${id}`)
-}
-
-const handlePassword = (text) =>{
-  console.log(text);
-  setPassword(text);
-  console.log(password)
-;}
-
-const handleUsername = (text) =>{
-  console.log(text);
-  setUsername(text);
-  console.log(username)
-}
   return (
     <View style={styles.container}>
-      <Text>Please Log in</Text>
-      <View style={styles.input_fields}>
-        <Text>Username</Text>
-        <TextInput 
-        onChangeText={(text)=>handleUsername(text)}
-        placeholder="username" 
-        id style={styles.text_input}
-        />
-      </View>
-      <View style={styles.input_fields}>
-        <Text>Password</Text>
-        <TextInput 
-        secureTextEntry={true} 
-        onChangeText={(text)=>{handlePassword(text)}}
-        placeholder="password" 
-        style={styles.text_input}/>
-      </View>
-      <Button color="red" title="Submit" onPress={()=>{handlePassword}}/>
-      <Text style={styles.usertext}>{username}</Text>
-      <Text style={styles.usertext}>{password}</Text>
-      <FlatList
-      keyExtractor={(item)=> item.id}
-        data={users}
-        renderItem={({item}) => (
-          <TouchableOpacity style={styles.usertext} onPress={()=>pressedButton(item.id)}>
-            <Text>{item.name}</Text>
-            <Text>{item.username}</Text>
-            <Text>{item.email}</Text>
-          </TouchableOpacity>
-        )}
-      />
-      <StatusBar style="auto" />
+      <Text>Hello World</Text>
+      
     </View>
   );
 }
@@ -75,32 +19,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text_input: {
-    borderWidth: 1,
-    borderColor: 'gray',
-    height: 40,
-    width: 200,
-    textAlign: "center"
-  },
-  input_fields:{
-    display: "flex",
-    flexDirection:"row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    width: 275,
-    marginTop: 10
-  },
-  usertext:{
-    borderWidth: 1,
-    borderColor: 'gray',
-    width: 200,
-    borderRadius: 5,
-    marginBottom: 5
-  },
-  submit:{
-    backgroundColor: 'red',
-    color: 'red',
-    borderWidth: 1,
-    borderColor: 'red'
-  }
 });
